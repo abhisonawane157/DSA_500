b1e1bc8f0be53c9da38a0cab020cee1ff79a4adf
akshatjain <akshatjaincs18@gmail.com>
2025-02-11 06:15:37 +0530

    -Elements left, + Elements right

--- Arrays/NegativeLeft.java
+++ Arrays/NegativeLeft.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
-
 public class NegativeLeft {
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    public static void move(int arr[]){
+        int l = 0 , r = arr.length - 1,mid = 0;
+        while(mid <= r){
+            if(arr[mid] < 0){
+                NegativeLeft.swap(arr, l, mid);
+                l++;
+                mid++;
+            }
+            else if(arr[mid]> 0 && arr[r]>0){
+                mid++;
+            }
+            else if(arr[mid]>0 && arr[r]<0){
+                NegativeLeft.swap(arr, r, mid);
+                r--;
+            }
+
+        }
+        for (int e : arr)
+            System.out.print(e + " ");
+
+    }
+    public static void main(String[] args)
+    {
+        int[] arr = { -1, 2, -3, 4, 5, 6, -7, 8, -9 };
+        NegativeLeft.move(arr.clone());
+//        for (int e : arr)
+//            System.out.print(e + " ");
+    }
 }
